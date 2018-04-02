1b495c66cb7a5be8d1a9203a2d80c85b08e849a8
Matt Beaney <mattbeaney@Matts-MacBook-Pro.local>
2018-04-02 14:40:12 +0100

    Master commit 1

--- BPCE/ViewController.swift
+++ BPCE/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib. Test.
+        // Do any additional setup after loading the view, typically from a nib. Test. Test 3.
     }
 
     override func didReceiveMemoryWarning() {
